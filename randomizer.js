57fef03d8af21f6e1c705f9aa770bdb274fd6091
FluxFlu <70078024+FluxFlu@users.noreply.github.com>
2023-01-24 18:29:05 -0500

    Update randomizer.js

--- randomizer.js
+++ randomizer.js
@@ -1,14 +1,29 @@
 #!/usr/bin/env node
 
 
-
 const settings = {
-    "gridItems": true,
+    "Grid Items": true,
 
-    "map": false,
-    "talking": false,
+    "Map": false,
+    "Talking Candy": false,
     "PLAY": false,
 
+}
+
+const settingEffects = {
+    
+    "Talking Candy": () => gridItems.push(`TalkingCandy`),
+
+    "Map": () => gridItems.push(`MainMap`),
+
+    "PLAY": () => {
+        gridItems.push(`P`);
+        gridItems.push(`L`);
+        gridItems.push(`A`);
+        gridItems.push(`Y`);
+    },
+    
+    "Grid Items": randomizeGridItems,
 }
 
 const readline = require('readline').createInterface({
@@ -64,47 +79,40 @@ function random_item(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-function replace_value(text, from, to) {
-    return text .replaceAll(gridItemEffect(from), "REPLACESTRING")
-                .replaceAll(gridItemEffect(to), gridItemEffectOut(from))
-                .replaceAll("REPLACESTRING", gridItemEffectOut(to))
-                
-                .replaceAll(gridItemVisuals(from), "REPLACESTRING")
-                .replaceAll(gridItemVisuals(to), gridItemVisualOut(from))
-                .replaceAll("REPLACESTRING", gridItemVisualOut(to))
+const g = {text: ""};
+
+function replace_value(from, to) {
+    g.text = g.text .replaceAll(gridItemEffect(from), "REPLACESTRING")
+                    .replaceAll(gridItemEffect(to), gridItemEffectOut(from))
+                    .replaceAll("REPLACESTRING", gridItemEffectOut(to))
+                    
+                    .replaceAll(gridItemVisuals(from), "REPLACESTRING")
+                    .replaceAll(gridItemVisuals(to), gridItemVisualOut(from))
+                    .replaceAll("REPLACESTRING", gridItemVisualOut(to))
 }
 
-function main() {
 
-    if (settings.map)
-        gridItems.push(`MainMap`);
-    
-    if (settings.talking)
-        gridItems.push(`TalkingCandy`);
-    
-    if (settings.PLAY) {
-        gridItems.push(`P`);
-        gridItems.push(`L`);
-        gridItems.push(`A`);
-        gridItems.push(`Y`);
-    }
-
-    let text = fs.readFileSync("./candybox2.js", "utf-8");
-    if (!text)
-        return console.log ("ERROR: Cannot find `./candybox2.js`.");
-
-    console.log("Randomizing...")
+function randomizeGridItems() {
     for (let i = 0; i < 60; i++) {
         const j = random_item(gridItems);
         const k = random_item(gridItems);
         if (j == k)
             continue;
-        if (settings.gridItems)
-            text = replace_value(text, j, k);
-
+        if (settings["Grid Items"])
+            replace_value(j, k);
     }
+}
+
+function main() {
+
+    g.text = fs.readFileSync("./candybox2.js", "utf-8");
+    if (!g.text)
+        return console.log ("ERROR: Cannot find `./candybox2.js`.");
+
+    console.log("Randomizing...")
+    Object.keys(settingEffects).forEach(e => { if (settings[e]) settingEffects[e]() });
     
-    fs.writeFileSync("./candybox2randomized.js", text);
+    fs.writeFileSync("./candybox2randomized.js", g.text);
     fs.writeFileSync("./index_randomized.html",  fs.readFileSync("./index.html", "utf-8").replaceAll("candybox2.js", "candybox2randomized.js"));
     console.log("Randomized and written to `./candybox2randomized.js`, run `index_randomized.html` to open the game.")
 }
